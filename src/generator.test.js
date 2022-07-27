edff4cf9e3ef9f6797d9a890be139362ba8d9ffe
Barry Coughlan <bcoughlan@protonmail.com>
2022-07-28 00:01:20 +0100

    Remove not very useful test

--- src/generator.test.js
+++ src/generator.test.js
@@ -3,12 +3,10 @@ const path = require('path')
 
 describe('snapshots', () => {
 
-  // This snapshot test is really just a way of making it easy for a developer to
-  // see the diff of their changes in a way that can be easily code reviewed.
-  it('generates petstore', async () => {
+  it('generates successfully', async () => {
     const inputFile = path.join(__dirname, '../test-resources/petstore.yaml')
     const output = await Generator(inputFile, 'petStore').generate()
-    expect(output).toMatchSnapshot()
+    expect(output).toContain('Everything about your Pets')
   })
 
 })
