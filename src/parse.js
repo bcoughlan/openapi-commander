6bab67ce3771a4488755ffba955fb6a856b155d8
Barry Coughlan <bcoughlan@protonmail.com>
2022-07-29 00:45:07 +0100

    Fix circular refs with swagger 2.0

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,12 @@
 const SwaggerParser = require('@apidevtools/swagger-parser')
 
 async function parseSpec(specLocation) {
-  const parsed = await SwaggerParser.dereference(specLocation)
+  const parsed = await SwaggerParser.parse(specLocation)
   if (parsed.swagger === '2.0') {
     const openapi = await convertSwaggerToOpenAPI(JSON.parse(JSON.stringify(parsed)))
-    console.log("HERE", openapi)
-    return await SwaggerParser.parse(openapi)
+    return SwaggerParser.dereference(openapi)
   }
-  return parsed
+  return SwaggerParser.dereference(parsed)
 }
 
 async function convertSwaggerToOpenAPI(swagger) {
