04810fe660c22c5e07e35d136c3268e409d6d6b3
Barry Coughlan <bcoughlan@protonmail.com>
2023-03-12 21:31:11 +0000

    Support dereferencing specs with relative file path

--- src/parse.ts
+++ src/parse.ts
@@ -1,25 +1,45 @@
 import SwaggerParser from '@apidevtools/swagger-parser'
 import { OpenAPI, OpenAPIV2 } from "openapi-types"
 import { OAS31Flat } from "./oas31-types"
+import path from 'path'
+
 
 /**
  * @returns a fully dereferenced OpenAPI 3.1 model
  * @param specLocation File path containing a Swagger 2, OpenAPI 3.0 or OpenAPI 3.1 YAML or JSON.
  */
-export async function parseSpec(specLocation : string) : Promise<OAS31Flat.Document> {
+export async function parseSpec(specLocation: string): Promise<OAS31Flat.Document> {
   const parsed = await SwaggerParser.parse(specLocation)
 
   //TODO convert OpenAPI 3.0 to OpenAPI 3.1 so we can work with a consistent format
 
   if ((parsed as OpenAPIV2.Document).swagger === '2.0') {
     const openapi = await convertSwaggerToOpenAPI(JSON.parse(JSON.stringify(parsed)))
-    return SwaggerParser.dereference(openapi as OpenAPI.Document) as Promise<OAS31Flat.Document>
+    return dereference(specLocation, openapi as OpenAPI.Document)
   }
-  return SwaggerParser.dereference(parsed) as Promise<OAS31Flat.Document>
-  
+  return dereference(specLocation, parsed)
+
 }
 
-async function convertSwaggerToOpenAPI(swagger : object) {
+async function dereference(specLocation: string, spec: OpenAPI.Document) : Promise<OAS31Flat.Document> {
+  if (specLocation.startsWith('http://') || specLocation.startsWith('https://')) {
+    return SwaggerParser.dereference(spec) as Promise<OAS31Flat.Document>
+  }
+
+  //Seems like SwaggerParser does not work with paths
+  //relative to the spec file, so we need to change the cwd
+  //as a workaround. 
+  const absoluteDir = path.dirname(path.resolve(specLocation))
+  const oldCwd = process.cwd()
+  try {
+    process.chdir(absoluteDir)
+    return SwaggerParser.dereference(spec) as Promise<OAS31Flat.Document>
+  } finally {
+    process.chdir(oldCwd)
+  }
+}
+
+async function convertSwaggerToOpenAPI(swagger: object) {
   const converter = await import('swagger2openapi')
   const options = { patch: true, warnOnly: true }
   return new Promise((resolve, reject) => {
